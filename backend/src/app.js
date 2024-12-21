7d82fb950979e6783573190a8110c8a339546b10
kim-dabin <pippisgoing@gmail.com>
2024-12-21 21:43:42 +0900

    port

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/nasa', nasaRoutes);
-// app.use('/api/weather', );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
